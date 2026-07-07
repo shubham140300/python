e6bb3a9a70cd24f15fb9c2ccfbd47d0464bfba8e
Shubham <shubham140300@gmail.com>
2026-07-07 10:03:27 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+
+import data
+#b = len(data.question_data)
+#print(b)
+#print(data.question_data)
+score=0
+question=1
+for a in data.question_data:
+    print(type(a))
+    print(a)
+    answer=input(f"{a['text']}  (True/False): ").capitalize()
+    if answer==a['answer']:
+        score +=1
+        print(score)
+        print("You got it right!")
+        print(f"The correct answer was: {a['answer']}.")
+        print(f"Your current score is: {score}/{question}")
+    elif answer!=a['answer']:
+        print("That's wrong.")
+        print(f"The correct answer was: {a['answer']}.")
+        print(f"Your current score is: {score}/{question}")
+    question+=question
+    
+  
